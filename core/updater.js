ea17c8816bc07f6f70551794dfc51ebd56da256e
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-03-27 20:32:52 +1100

    better changelog

--- core/updater.js
+++ core/updater.js
@@ -1,6 +1,6 @@
 const { info } = require("../package.json");
 const request = require("./request");
-const logger = require("./logger")
+const logger = require("./logger");
 const path = require("path");
 const { ipcRenderer, shell } = require("electron");
 
@@ -47,12 +47,12 @@ async function failModal(title, content) {
 }
 
 async function checkForUpdates() {
-    logger.log("Velocity", "Checking for updates")
+    logger.log("Velocity", "Checking for updates");
     let updateData;
     request(updateURL, (_, __, body) => (updateData = JSON.parse(body)));
 
-    await waitUntil(() => window.document.querySelector('[class*="guilds"]'))
-    VApi.showToast("Requesting Update Data")
+    await waitUntil(() => window.document.querySelector('[class*="guilds"]'));
+    VApi.showToast("Requesting Update Data");
     setTimeout(() => {
         if (updateData) {
             if (updateData.version !== info.version) {
@@ -81,8 +81,8 @@ async function checkForUpdates() {
 
                                             const VDir = path.join(__dirname, "..");
 
-                                            let targetPackage
-                                            showToast("Requesting package...")
+                                            let targetPackage;
+                                            showToast("Requesting package...");
                                             request("https://raw.githubusercontent.com/Velocity-Discord/Velocity/main/package.json", (err, _, body) => {
                                                 if (err) {
                                                     showToast("Request Failed", { type: "error" });
@@ -95,18 +95,22 @@ async function checkForUpdates() {
                                                             "You can manually update Velocity by opening the Velocity Folder and doing one of the following,",
                                                             "- Run `git pull` in the terminal (inside the folder)",
                                                             "- Download the **ZIP** from GitHub and replace the old folder with it uncompressed.",
-                                                            React.createElement(ButtonEle, {
+                                                            React.createElement(
+                                                                ButtonEle,
+                                                                {
                                                                     id: "velocity-folder",
                                                                     color: Button.ButtonColors.BRAND,
                                                                     className: ["velocity-button"],
                                                                     onClick: () => {
                                                                         shell.openPath(VDir);
                                                                     },
-                                                                }, "Open Velocity Folder"),
+                                                                },
+                                                                "Open Velocity Folder",
+                                                            ),
                                                         ]);
                                                     }
                                                 }
-                                            })
+                                            });
 
                                             // changelogModal({ subtitle: updateData.changelog.subtitle, description: updateData.changelog.description });
                                         },
@@ -139,10 +143,10 @@ async function checkForUpdates() {
 
                 updatePrompt();
             } else {
-                VApi.showToast("No Updates Found")
+                VApi.showToast("No Updates Found");
             }
         }
-    }, 1500)
+    }, 1500);
 }
 
 async function changelogModal(options = {}) {
@@ -150,43 +154,82 @@ async function changelogModal(options = {}) {
     request(updateURL, (_, __, body) => (updateJson = JSON.parse(body)));
 
     setTimeout(() => {
-    const { image = updateJson.changelog.image || "https://velocity-discord.netlify.app/assets/3.png", subtitle = updateJson.changelog.subtitle, description = updateJson.changelog.description } = options;
-    const { React, getModule, modals } = VApi;
-    const ConfirmationModal = getModule.find("ConfirmModal").default;
-    const Button = getModule.find(["ButtonColors"]);
-    const ChangelogClasses = getModule.find(["fixed", "improved"]);
-    const ModalCloseButton = getModule.find(["ModalCloseButton"]).default;
-    const Text = getModule.find("Text").default;
-    const Titles = getModule.find(["Tags", "default"]);
-    const dateClass = VApi.getModule.find(["size12", "size32"]).size12;
-    
-    const { Messages } = getModule.find((m) => m.default?.Messages?.OKAY).default;
-    const Markdown = VApi.getModule.find((m) => m.default?.displayName === "Markdown" && m.default.rules).default;
+        const {
+            image = updateJson.changelog.image || "https://velocity-discord.netlify.app/assets/3.png",
+            subtitle = updateJson.changelog.subtitle,
+            description = updateJson.changelog.description,
+        } = options;
+        const { React, getModule, modals } = VApi;
+        const ConfirmationModal = getModule.find("ConfirmModal").default;
+        const Button = getModule.find(["ButtonColors"]);
+        const ChangelogClasses = getModule.find(["fixed", "improved"]);
+        const Text = getModule.find("Text").default;
+        const Titles = getModule.find(["Tags", "default"]);
+        const dateClass = getModule.find(["size12", "size32"]).size12;
+        const closeModals = getModule.find(["closeAllModals"]).closeAllModals;
+        const closeClasses = getModule.find(["root", "close"]);
+        const contentsClasses = getModule.find(["spinnerItem", "submitting"]);
 
-    return new Promise((resolve) => {
-        modals.open((props) => {
-            if (props.transitionState === 3) resolve(false);
-            return React.createElement(
-                ConfirmationModal,
-                Object.assign(
-                    {
-                        bodyClassName: "velocity-changelog",
-                        header: [
-                            React.createElement("h2", null, "What's New"),
-                            React.createElement("div", { style: { fontWeight: "400" }, className: `${dateClass} ${ChangelogClasses.date}` }, subtitle),
-                        ],
-                        onCancel: () => resolve(false),
-                        children: [
-                            React.createElement("img", {src: image}),
-                            React.createElement(Markdown, { className: Text.Colors.HEADER_SECONDARY }, description)
-                        ],
-                    },
-                    props,
-                ),
-            );
+        const { Messages } = getModule.find((m) => m.default?.Messages?.OKAY).default;
+        const Markdown = getModule.find((m) => m.default?.displayName === "Markdown" && m.default.rules).default;
+
+        return new Promise((resolve) => {
+            modals.open((props) => {
+                if (props.transitionState === 3) resolve(false);
+                return React.createElement(
+                    ConfirmationModal,
+                    Object.assign(
+                        {
+                            bodyClassName: "velocity-changelog",
+                            header: [
+                                React.createElement("div", {
+                                    class: "velocity-modal-header-flex",
+                                    children: [
+                                        React.createElement("div", {
+                                            class: "velocity-modal-header-container",
+                                            children: [
+                                                React.createElement("h2", null, "What's New"),
+                                                React.createElement("div", { style: { fontWeight: "400" }, className: `${dateClass} ${ChangelogClasses.date}` }, subtitle),
+                                            ],
+                                        }),
+                                        React.createElement("button", {
+                                            type: "button",
+                                            className: `${closeClasses.close} ${contentsClasses.button} ${contentsClasses.lookBlank} ${contentsClasses.colorBrand} ${contentsClasses.grow}`,
+                                            onClick: () => {
+                                                closeModals();
+                                            },
+                                            children: [
+                                                React.createElement("div", {
+                                                    class: `${contentsClasses.contents}`,
+                                                    children: [
+                                                        React.createElement("svg", {
+                                                            class: `${closeClasses.closeIcon}`,
+                                                            width: "24",
+                                                            height: "24",
+                                                            viewBox: "0 0 24 24",
+                                                            children: [
+                                                                React.createElement("path", {
+                                                                    fill: "currentColor",
+                                                                    d: "M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z",
+                                                                }),
+                                                            ],
+                                                        }),
+                                                    ],
+                                                }),
+                                            ],
+                                        }),
+                                    ],
+                                }),
+                            ],
+                            onCancel: () => resolve(false),
+                            children: [React.createElement("img", { src: image }), React.createElement(Markdown, { className: Text.Colors.HEADER_SECONDARY }, description)],
+                        },
+                        props,
+                    ),
+                );
+            });
         });
-    });
-    }, 1500)
+    }, 1500);
 }
 
 module.exports = { checkForUpdates, changelogModal };
