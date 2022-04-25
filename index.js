428762de017932c125a095050d1ced27b4888ca6
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-04-25 11:20:20 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// This Client Mod is based on DrDiscord version 1.0.0 as found here: https://github.com/Dr-Discord/DrDiscord (now unavailable)
+// This Client Mod is based on DrDiscord version 1.0.0 as found here: https://github.com/Dr-Discord/DrDiscord (now unavailable, an archived version can be found here https://github.com/unknown81311/DrDiscord)
 // Original license: MIT License
-// hhttps://github.com/Dr-Discord/DrDiscord/blob/main/LICENSE.md
+// hhttps://github.com/Dr-Discord/DrDiscord/blob/main/LICENSE.md (now: https://github.com/unknown81311/DrDiscord/blob/main/LICENSE.md)
 
 const { join } = require("path");
 const electron = ({ ipcMain, app } = require("electron"));
